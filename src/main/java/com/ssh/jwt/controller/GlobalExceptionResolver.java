f25f9c5d377580f5e368212cfbd932fe33c5fd07
lunjingjie <lunjj@create80.com>
2018-08-07 17:28:59 +0800

    add check pageResource ids controller

--- src/main/java/com/ssh/jwt/controller/GlobalExceptionResolver.java
+++ src/main/java/com/ssh/jwt/controller/GlobalExceptionResolver.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@ControllerAdvice
 public class GlobalExceptionResolver extends DefaultHandlerExceptionResolver {
 
     public static Logger logger = Logger.getLogger("AllError");
